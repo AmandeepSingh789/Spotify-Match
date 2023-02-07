c91545f10c2f5ae84787d84556a42ab26e08db87
Amaan Sheikh <amaan784@gmail.com>
2023-02-07 03:48:00 -0800

    minor button change

--- screens/MatchesScreen.js
+++ screens/MatchesScreen.js
@@ -9,8 +9,8 @@ export default function MatchesScreen({ navigation }) {
             <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Matches Screen</Text>
-
-        <View style={{ justifyContent: 'space-between', alignItems: 'center',
+        
+        <View style={{ justifyContent: 'space-between', alignItems: 'flex-end',
                         flexDirection: 'row', paddingHorizontal: '15%', }}>
             <Icon
             raised
@@ -28,63 +28,6 @@ export default function MatchesScreen({ navigation }) {
             onPress={() => console.log('like')}
             />
         </View>
-        </View>
-                    
+        </View>           
         );
 }
-
-// import React from 'react'
-// import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-// import { func, string } from 'prop-types'
-// import Icon from 'react-native-vector-icons/AntDesign'
-
-
-// const IconButton = ({ onPress, name, backgroundColor, color }) => (
-//   <TouchableOpacity
-//     style={[styles.singleButton, { backgroundColor }]}
-//     onPress={onPress}
-//     activeOpacity={0.85}
-//   >
-//     <Icon
-//       name={name}
-//       size={20}
-//       color={color}
-//     />
-//   </TouchableOpacity>
-// )
-// IconButton.defaultProps = {
-//   color: '#fff',
-//   backgroundColor: '#3CA3FF',
-// }
-// IconButton.propTypes = {
-//   onPress: func.isRequired,
-//   name: string.isRequired,
-//   color: string,
-//   backgroundColor: string,
-// }
-
-// const styles = StyleSheet.create({
-//     singleButton: {
-//         backgroundColor: 'transparent',
-//         borderRadius: 50,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         shadowColor: 'black',
-//         shadowOffset: {
-//           width: 0,
-//           height: 2,
-//         },
-//         shadowRadius: 6,
-//         shadowOpacity: 0.3,
-//         elevation: 2,
-//         padding: 15,
-//       },
-//     buttonsContainer: {
-//         justifyContent: 'space-between',
-//         alignItems: 'center',
-//         flexDirection: 'row',
-//         paddingHorizontal: '15%',
-//       },  
-// });
-
-// export default IconButton
