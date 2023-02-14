e8228362e7dbf4e1e08f6ad20340cf666d86d874
Vani Agrawal <vani.agrawal@gmail.com>
2023-02-13 16:16:57 -0800

    matches page

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import UserCard from './components/UserCard';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import UserCard from "./components/UserCard";
+import ChatScreen from "./screens/ChatScreen";
+import MatchScreen from "./screens/MatchScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    
-    <View style={styles.container}>
-    
-    <UserCard />
-    
-
-    
-
-    </View>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="MatchScreen"
+          component={MatchScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="ChatScreen"
+          component={ChatScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    
-    // height:"0%",    
+    backgroundColor: "black",
   },
-  
 });
--- README.md
+++ README.md
@@ -7,9 +7,14 @@
 - Calder
 - Rami
 
-
-# # Dependencies 
+# # Dependencies
 
     rneui/base
     rneui/themed
     expo-font
+    @react-navigation/native
+    @react-navigation/stack
+    expo-linear-gradient
+    react-native-gesture-handler
+    react-native-safe-area-context
+    react-native-screens
