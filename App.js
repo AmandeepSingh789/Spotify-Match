faf6f744fb3bb622036a0f7b281f2e3aeb42954e
Amaan Sheikh <amaan784@gmail.com>
2023-03-02 19:31:16 -0800

    added old navigation code

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import SpotifyLogin from './screens/SpotifyLoginScreen';
 import SurveyGeneralQuestions from './screens/SurveyGeneralQuestionsScreen';
@@ -11,7 +10,6 @@ import SurveyBio from './screens/SurveyBioScreen';
 import SurveyAdvancedQuestions from './screens/SurveyAdvancedQuestionsScreen';
 import AddImages from './screens/AddImagesScreen';
 import Home from './screens/HomeScreen';
-
 import Matches from './screens/MatchesScreen';
 import Profile from './screens/ProfileScreen';
 
@@ -34,10 +32,47 @@ export default function App() {
 function TabNavigation() {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Homepage" component={Home} />
-      <Tab.Screen name="Matches" component={Matches} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      initialRouteName={"Homepage"}
+          // initialRouteName={Home}
+          screenOptions={({ route }) => ({
+              "tabBarActiveTintColor": "tomato",
+              "tabBarInactiveTintColor": "grey",
+              "tabBarLabelStyle": {
+              "paddingBottom": 3,
+              "fontSize": 9,
+              // "paddingBottom": 10,
+              // "fontSize": 10
+          },
+          headerShown: false, // this controls the header, commenting this out will show the header
+          // headerStyle: { backgroundColor: 'black' },
+          "tabBarStyle": [
+              {
+              // "backgroundColor": "#28282B",  
+              "display": "flex",
+              // "height": 75,
+              },
+              null
+          ],
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+            // if (rn === Home) {
+            if (rn === "Homepage") {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (rn === "Matches") {
+              iconName = focused ? 'chatbox' : 'chatbox-outline';
+            } else if (rn === "Profile") {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          >
+        <Tab.Screen options={{headerShown: false}} name="Homepage" component={Home} />
+        <Tab.Screen name="Matches" component={Matches} />
+        <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 }
--- components/Navigator.js
+++ components/Navigator.js
@@ -1,77 +1,77 @@
-// import * as React from 'react';
-// import { NavigationContainer, DefaultTheme, DarkTheme, } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-// // import { useColorScheme } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer, DefaultTheme, DarkTheme, } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { useColorScheme } from 'react-native';
 
-// // Screens
-// import HomeScreen from '../screens/HomeScreen';
-// import MatchesScreen from '../screens/MatchesScreen';
-// import ProfileScreen from '../screens/ProfileScreen';
+// Screens
+import HomeScreen from '../screens/HomeScreen';
+import MatchesScreen from '../screens/MatchesScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
-// //Screen names
-// const homeName = "Homepage";
-// const matchesName = "Matches";
-// const profileName = "Profile";
+//Screen names
+const homeName = "Homepage";
+const matchesName = "Matches";
+const profileName = "Profile";
 
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-// function MainContainer() {
-//   // const scheme = useColorScheme();
-//   return (
+function MainContainer() {
+  // const scheme = useColorScheme();
+  return (
 
-//     <NavigationContainer theme={DarkTheme} independent={true}>
-//     {/* <NavigationContainer theme={DarkTheme}> */}
-//       <Tab.Navigator
-//         initialRouteName={homeName}
-//         // initialRouteName={Home}
-//         screenOptions={({ route }) => ({
-//             "tabBarActiveTintColor": "tomato",
-//             "tabBarInactiveTintColor": "grey",
-//             "tabBarLabelStyle": {
-//             "paddingBottom": 3,
-//             "fontSize": 9,
-//             // "paddingBottom": 10,
-//             // "fontSize": 10
-//         },
-//         headerShown: false, // this controls the header, commenting this out will show the header
-//         // headerStyle: { backgroundColor: 'black' },
-//         "tabBarStyle": [
-//             {
-//             // "backgroundColor": "#28282B",  
-//             "display": "flex",
-//             // "height": 75,
-//             },
-//             null
-//         ],
-//           tabBarIcon: ({ focused, color, size }) => {
-//             let iconName;
-//             let rn = route.name;
-//             // if (rn === Home) {
-//             if (rn === homeName) {
-//               iconName = focused ? 'person' : 'person-outline';
+    <NavigationContainer theme={DarkTheme} independent={true}>
+    {/* <NavigationContainer theme={DarkTheme}> */}
+      <Tab.Navigator
+        initialRouteName={homeName}
+        // initialRouteName={Home}
+        screenOptions={({ route }) => ({
+            "tabBarActiveTintColor": "tomato",
+            "tabBarInactiveTintColor": "grey",
+            "tabBarLabelStyle": {
+            "paddingBottom": 3,
+            "fontSize": 9,
+            // "paddingBottom": 10,
+            // "fontSize": 10
+        },
+        headerShown: false, // this controls the header, commenting this out will show the header
+        // headerStyle: { backgroundColor: 'black' },
+        "tabBarStyle": [
+            {
+            // "backgroundColor": "#28282B",  
+            "display": "flex",
+            // "height": 75,
+            },
+            null
+        ],
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+            // if (rn === Home) {
+            if (rn === homeName) {
+              iconName = focused ? 'person' : 'person-outline';
 
-//             } else if (rn === matchesName) {
-//               iconName = focused ? 'chatbox' : 'chatbox-outline';
+            } else if (rn === matchesName) {
+              iconName = focused ? 'chatbox' : 'chatbox-outline';
 
-//             } else if (rn === profileName) {
-//               iconName = focused ? 'person-circle' : 'person-circle-outline';
-//             }
+            } else if (rn === profileName) {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            }
 
-//             // You can return any component that you like here!
-//             return <Ionicons name={iconName} size={size} color={color} />;
-//           },
-//         })}
-//         >
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        >
 
-//         {/* <Tab.Screen name={homeName} component={HomeScreen} /> */}
-//         <Tab.Screen name={matchesName} component={MatchesScreen} />
-//         <Tab.Screen name={profileName} component={ProfileScreen} />
+        {/* <Tab.Screen name={homeName} component={HomeScreen} /> */}
+        <Tab.Screen name={matchesName} component={MatchesScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
         
 
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
-// export default MainContainer;
+export default MainContainer;
